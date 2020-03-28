624051c83497717c4dbd2cbb37e9b7fa89ff38d0
biryuk <biryuk@exchange.informatik.uni-hamburg.de>
2020-03-28 10:59:28 +0100

    ADD: PROGRESS INDICATOR WIDGET

---
+++ app/covsense/lib/widgets/progress_indicator_widget.dart
@@ -0,0 +1,58 @@
+// TODO: Finish implementation
+/* import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class ProgressIndicatorWidget extends StatelessWidget {
+  final GlobalKey fbKey;
+  final List<String> progress;
+
+  ProgressIndicatorWidget({
+    Key key,
+    @required this.fbKey,
+    this.progress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        flex: 1,
+        child: Column(
+          children: progress.map(progressState){
+            if (progressState) {
+              
+            }
+          } <Widget>[
+            Icon(
+              Icons.check_circle,
+              size: 30,
+              color: Theme.of(context).primaryColorLight,
+            ),
+            SizedBox(height: 20),
+            Icon(
+              Icons.radio_button_unchecked,
+              size: 30,
+              color: Theme.of(context).primaryColor,
+            ),
+            SizedBox(height: 20),
+            Icon(
+              Icons.radio_button_unchecked,
+              size: 30,
+              color: Theme.of(context).primaryColor,
+            ),
+            SizedBox(height: 20),
+            Icon(
+              Icons.radio_button_unchecked,
+              size: 30,
+              color: Theme.of(context).primaryColor,
+            ),
+            SizedBox(height: 20),
+            Icon(
+              Icons.radio_button_unchecked,
+              size: 30,
+              color: Theme.of(context).primaryColor,
+            ),
+          ],
+        ));
+  }
+}
+ */
