f4af5472b81bbe9abd2aa9e0fe221a0d273df7f7
biryuk <biryuk@exchange.informatik.uni-hamburg.de>
2020-03-29 21:10:59 +0200

    UPDATE: IMPLEMENT PROGRESS INDICATOR WIDGET

--- app/covsense/lib/widgets/progress_indicator_widget.dart
+++ app/covsense/lib/widgets/progress_indicator_widget.dart
@@ -1,5 +1,5 @@
 // TODO: Finish implementation
-/* import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class ProgressIndicatorWidget extends StatelessWidget {
@@ -14,45 +14,44 @@ class ProgressIndicatorWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-        flex: 1,
-        child: Column(
-          children: progress.map(progressState){
-            if (progressState) {
-              
-            }
-          } <Widget>[
-            Icon(
-              Icons.check_circle,
-              size: 30,
-              color: Theme.of(context).primaryColorLight,
-            ),
-            SizedBox(height: 20),
-            Icon(
-              Icons.radio_button_unchecked,
-              size: 30,
-              color: Theme.of(context).primaryColor,
-            ),
-            SizedBox(height: 20),
-            Icon(
-              Icons.radio_button_unchecked,
-              size: 30,
-              color: Theme.of(context).primaryColor,
-            ),
-            SizedBox(height: 20),
-            Icon(
-              Icons.radio_button_unchecked,
-              size: 30,
-              color: Theme.of(context).primaryColor,
-            ),
-            SizedBox(height: 20),
-            Icon(
-              Icons.radio_button_unchecked,
-              size: 30,
-              color: Theme.of(context).primaryColor,
-            ),
-          ],
-        ));
+    return Column(children: generateIcons(progress, context));
+  }
+
+  /// Generate the icons for the progress bar according to the progress item strings.
+  List<Widget> generateIcons(List<String> progressItems, BuildContext context) {
+    List<Widget> icons = <Widget>[];
+    for (var progressItem in progressItems) {
+      icons.add(generateIcon(progressItem, context));
+      icons.add(SizedBox(height: 16));
+    }
+    return icons;
+  }
+
+  /// Generate the appropriate icon according to the progress item string passed.
+  Icon generateIcon(String progressItem, BuildContext context) {
+    Icon icon;
+    switch (progressItem) {
+      case 'done':
+        icon = Icon(
+          Icons.check_circle,
+          size: 30,
+          color: Theme.of(context).primaryColor,
+        );
+        break;
+      case 'doing':
+        icon = Icon(
+          Icons.check_circle,
+          size: 30,
+          color: Theme.of(context).primaryColorLight,
+        );
+        break;
+      default:
+        icon = Icon(
+          Icons.radio_button_unchecked,
+          size: 30,
+          color: Theme.of(context).primaryColor,
+        );
+    }
+    return icon;
   }
 }
- */
